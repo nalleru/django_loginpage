1c44b9f41cf4fe866e85c40d3b7773af3414fc34
nalleru <nalleru.neelima@gmail.com>
2022-07-13 16:25:06 +0530

    first commit

---
+++ first_project/base/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
+
+
+class Customlogin(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
